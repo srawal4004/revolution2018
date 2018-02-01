bcb4ad2366e5f535826959e9d08f483fc0b217bb
srawal4004 <kishan_comp@MacBook-Air.local>
2018-02-01 11:59:02 +0530

    Initial Commit

---
+++ revolution2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  revolution2018
+//
+//  Created by Srusti Rawal on 2018-02-01.
+//  Copyright © 2018 Motifit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
